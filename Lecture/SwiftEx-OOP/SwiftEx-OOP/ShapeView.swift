7301932991c04c7141db57563c75096699a2db04
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-06 14:49:36 +0900

    #1 let 변수 유지하도록 코드 리팩토링

--- Lecture/SwiftEx-OOP/SwiftEx-OOP/ShapeView.swift
+++ Lecture/SwiftEx-OOP/SwiftEx-OOP/ShapeView.swift
@@ -9,51 +9,59 @@ import Foundation
 
 class ShapeView {
 
-    var shapeList: [Shape]
+//    var shapeList: [Shape]
+    let shapeList: [Shape]
 
     init() {
         // init()함수의 목적은 초기화
         shapeList = []
     }
 
-    func showList() {
-        for shape in shapeList {
+//    func showList() {
+//        for shape in shapeList {
+//            shape.draw()
+//        }
+//    }
+
+//    func appendShpe(choice: Int) {
+//        if choice == 1 {
+//            shapeList.append(Circle())
+//        } else if choice == 2 {
+//            shapeList.append(Rectangle())
+//        }
+//    }
+    
+    func showList(_ list: [Shape]) {
+        for shape in list {
             shape.draw()
         }
     }
-
-    func appendShpe(choice: Int) {
+    
+    func appendShape(to list: [Shape], choice: Int) -> [Shape] {
+        var newList = list
         if choice == 1 {
-            shapeList.append(Circle())
+            newList.append(Circle())
         } else if choice == 2 {
-            shapeList.append(Rectangle())
+            newList.append(Rectangle())
         }
+        return newList
     }
 
     func main() {
-
-        // 5회 반복 Shape 생성
-        // 1번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle: 1
-        // 리스트에 Circle 객체 추가 완료!
-        // 2번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle: 2
-        // 리스트에 Rectangle 객체 추가 완료!
-        // 3번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle:
-
+        var currentList = shapeList
+        
         for i in 1...5 {
-            print("\(i)번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle", terminator: ": ")
+            print("\(i)번째 생성할 객체 타입 선택 (1)Circle (2)Rectangle", terminator: ": ")
             let choice: Int = Int(readLine() ?? "") ?? 0
+            
             switch choice {
-            case 1:
-                appendShpe(choice: choice)
-            case 2:
-                appendShpe(choice: choice)
+            case 1, 2:
+                currentList = appendShape(to: currentList, choice: choice)
             default:
                 print("잘못된 객체 타입입니다.")
             }
-            
         }
-
-        showList()
-
+        
+        showList(currentList)
     }
 }
