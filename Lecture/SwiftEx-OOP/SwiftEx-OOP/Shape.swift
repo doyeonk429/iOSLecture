b5ac6c8e7b695cc61f6dde0256394f0b9c5cfccf
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-06 14:38:21 +0900

    #1 OOP 실습 코드 메뉴 선택 기능 추가

--- Lecture/SwiftEx-OOP/SwiftEx-OOP/Shape.swift
+++ Lecture/SwiftEx-OOP/SwiftEx-OOP/Shape.swift
@@ -11,7 +11,7 @@ class Shape {
     //MARK: - Properties
     var shapeName : String
     
-    init(shapeName: String) {
+    init(shapeName: String = "") {
         self.shapeName = shapeName
     }
     
@@ -30,5 +30,57 @@ class Circle : Shape {
 class Rectangle : Shape {
     override func draw() {
         print("draw a rectangle🟦")
+    }
+}
+
+
+class ShapeView {
+
+    var shapeList: [Shape]
+
+    init() {
+        // init()함수의 목적은 초기화
+        shapeList = []
+    }
+
+    func showList() {
+        for shape in shapeList {
+            shape.draw()
+        }
+    }
+
+    func appendShpe(choice: Int) {
+        if choice == 1 {
+            shapeList.append(Circle())
+        } else if choice == 2 {
+            shapeList.append(Rectangle())
+        }
+    }
+
+    func main() {
+
+        // 5회 반복 Shape 생성
+        // 1번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle: 1
+        // 리스트에 Circle 객체 추가 완료!
+        // 2번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle: 2
+        // 리스트에 Rectangle 객체 추가 완료!
+        // 3번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle:
+
+        for i in 1...5 {
+            print("(i)번째 생성 할 객체 타입 선택 (1)Circle (2)Rectangle", terminator: ": ")
+            let choice: Int = Int(readLine() ?? "") ?? 0
+            switch choice {
+            case 1:
+                appendShpe(choice: choice)
+            case 2:
+                appendShpe(choice: choice)
+            default:
+                print("잘못된 객체 타입입니다.")
+            }
+            
+        }
+
+        showList()
+
     }
 }
