e7ecaa19f171526ada1b2da684b293b3a358af1a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-06 13:10:59 +0900

    [Lecture] 도형 그리기 실습

---
+++ Lecture/SwiftEx-OOP/SwiftEx-OOP/Shape.swift
@@ -0,0 +1,34 @@
+//
+//  Shape.swift
+//  SwiftEx-OOP
+//
+//  Created by 김도연 on 2/6/25.
+//
+
+import Foundation
+
+class Shape {
+    //MARK: - Properties
+    var shapeName : String
+    
+    init(shapeName: String) {
+        self.shapeName = shapeName
+    }
+    
+    //MARK: - Methods
+    func draw() {
+        print("draw a shape")
+    }
+}
+
+class Circle : Shape {
+    override func draw() {
+        print("draw a circle🔵")
+    }
+}
+
+class Rectangle : Shape {
+    override func draw() {
+        print("draw a rectangle🟦")
+    }
+}
--- Lecture/SwiftEx-OOP/SwiftEx-OOP/main.swift
+++ Lecture/SwiftEx-OOP/SwiftEx-OOP/main.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 
+//MARK: - Cars
+//let sonata = Car(color: "검정", speed: 110)
+//let grandeur = Car(color: "하얀색", speed: 140)
+//
+//let cars = [sonata, grandeur]
+//
+//for car in cars {
+//    car.drive()
+//}
+
+//MARK: - Shape
+var shapeArr = [Shape]()
+shapeArr.append(Circle(shapeName: "원"))
+shapeArr.append(Rectangle(shapeName: "사각형"))
+
