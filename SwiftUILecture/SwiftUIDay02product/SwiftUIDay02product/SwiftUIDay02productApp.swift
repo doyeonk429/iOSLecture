c53f925a6eec47d8119d152dccdd4ed34390a56e
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-11 15:23:43 +0900

    #1 [SwiftUI] 기초부터 다시 만들기 실습

---
+++ SwiftUILecture/SwiftUIDay02product/SwiftUIDay02product/SwiftUIDay02productApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDay02productApp.swift
+//  SwiftUIDay02product
+//
+//  Created by 김도연 on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDay02productApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
