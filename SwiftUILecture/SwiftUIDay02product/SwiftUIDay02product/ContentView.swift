393564b00b833940ca528a0ee985ba95e8cbd0d7
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-11 15:23:43 +0900

    #1 [SwiftUI] 기초부터 다시 만들기 실습

---
+++ SwiftUILecture/SwiftUIDay02product/SwiftUIDay02product/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIDay02product
+//
+//  Created by 김도연 on 2/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
