2c13daf093679cb5befa4b9a64fa8ec77f05ec06
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-28 14:16:13 +0900

    #5 code-based UI 세팅 + 라이브러리 세팅

--- UIKitLecture/UIKit-Movie/UIKit-Movie/ViewController.swift
+++ UIKitLecture/UIKit-Movie/UIKit-Movie/ViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import Combine
 
 class ViewController: UIViewController {
+    private var collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
